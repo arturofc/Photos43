d672fade2a7fe6dcb4e6f77ff3e311a19e4e5c4c
califlower <cal13199@gmail.com>
2017-04-10 14:39:05 -0400

    Scrollpane

--- src/application/Photos.java
+++ src/application/Photos.java
@@ -1,6 +1,9 @@
 package application;
 
 import Controllers.LoginController;
+import Models.Album;
+import Models.Photo;
+import Models.User;
 import com.sun.javafx.stage.StageHelper;
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -14,7 +17,9 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 
 /**
  * @author Calin Gilan
@@ -90,6 +95,41 @@ public class Photos extends Application
      */
     public static void main(String[] args)
     {
+        /*
+        User u = User.getUser("stock").get();
+
+
+        File f1 = new File("stockPhotos/image1.jpg");
+        File f2 = new File("stockPhotos/image2.png");
+        File f3 = new File("stockPhotos/image3.jpg");
+        File f4 = new File("stockPhotos/image4.jpg");
+        File f5 = new File("stockPhotos/image5.jpg");
+        File f6 = new File("stockPhotos/image6.jpg");
+        File f7 = new File("stockPhotos/image7.jpg");
+
+        Photo p1 = new Photo("image1", f1);
+        Photo p2 = new Photo("image2", f2);
+        Photo p3 = new Photo("image3", f3);
+        Photo p4 = new Photo("image4", f4);
+        Photo p5 = new Photo("image5", f5);
+        Photo p6 = new Photo("image6", f6);
+        Photo p7 = new Photo("image7", f7);
+
+        HashSet<Photo> pSet = new HashSet<>();
+
+        pSet.add(p1);
+        pSet.add(p2);
+        pSet.add(p3);
+        pSet.add(p4);
+        pSet.add(p5);
+        pSet.add(p6);
+        pSet.add(p7);
+
+        Album a = new Album("stock", u);
+        a.setPhotos(pSet);
+        Album.commitAlbum(a);
+        */
+
         launch(args);
     }
 
